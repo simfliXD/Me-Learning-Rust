1e1b1c7018f63aa0d63802e8ae5ae5485e1bf817
simfliXD <flinksimon@hotmail.com>
2026-05-23 00:37:19 +0200

    Delete HelloWorld!/helloWorld/target/debug directory

--- HelloWorld!/helloWorld/target/debug/deps/helloWorld-c34e4884480c0f1e.d
+++
@@ -1,5 +0,0 @@
-/home/simfli/Desktop/Rust/HelloWorld!/helloWorld/target/debug/deps/helloWorld-c34e4884480c0f1e.d: src/main.rs
-
-/home/simfli/Desktop/Rust/HelloWorld!/helloWorld/target/debug/deps/libhelloWorld-c34e4884480c0f1e.rmeta: src/main.rs
-
-src/main.rs:
--- HelloWorld!/helloWorld/target/debug/helloWorld.d
+++
@@ -1,1 +0,0 @@
-/home/simfli/Desktop/Rust/HelloWorld!/helloWorld/target/debug/helloWorld: /home/simfli/Desktop/Rust/HelloWorld!/helloWorld/src/main.rs
