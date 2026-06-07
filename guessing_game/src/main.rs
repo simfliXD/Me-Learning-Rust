6ede2d66427d5648af80049305c243f3fc71b03f
simfli <dollop-vacant-hula@duck.com>
2026-06-07 12:25:53 +0200

    clearer code with improvements

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,61 +2,81 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+// Secret number range
+const MIN_NUMBER: u32 = 1;
+const MAX_NUMBER: u32 = 100;
 const MAX_ATTEMPTS: u32 = 10;
+
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(MIN_NUMBER..=MAX_NUMBER);
 
     let mut attempts: u32 = 0;
 
-    let mut guess: String = String::new();
+    let mut input: String = String::new();
     let mut guessed_numbers: Vec<u32> = Vec::new();
 
     println!("Guess the number!");
 
     loop {
-        guess.clear();
+        input.clear();
 
-        println!("You have {} attempts left.", MAX_ATTEMPTS - attempts);
-        println!("Please input your guess.");
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read guess");
-
-        println!("Your guess is: {guess}");
-
-        // Trim whitespace and remove "\n" from enter press then parse to unsigned intiger, if err prompt to type an intiger.
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num, // Return the formated guess
-            Err(_) => {
-                println!("Please type an positive integer!");
-                continue;
-            }
-        };
-
-        attempts += 1;
         if attempts >= MAX_ATTEMPTS {
             println!(
-                "You lose, because you reached max attempts {}",
+                "You lose, because you reached max attempts ({})",
                 MAX_ATTEMPTS
             );
             println!("The secret number was {secret_number}!");
             break;
+        } else {
+            println!("You have {} attempts left.", MAX_ATTEMPTS - attempts);
         }
 
+        println!("Please input your guess.");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read guess.");
+
+        // Trim whitespace and remove "\n" from enter press then parse to unsigned 32bit intiger, if err prompt to type an intiger.
+        let guess: u32 = match input.trim().parse::<u32>() {
+            // No error and is within bounds
+            Ok(num) if (MIN_NUMBER..=MAX_NUMBER).contains(&num) => num,
+            //
+            Ok(num) => {
+                println!(
+                    "Guess is out of bounds: {} (must be between {} and {})",
+                    num, MIN_NUMBER, MAX_NUMBER
+                );
+                continue;
+            }
+            Err(_) => {
+                println!("Please type a valid integer.");
+                continue;
+            }
+        };
+
+        if guessed_numbers.contains(&guess) {
+            println!("You've already guessed that number\n");
+            continue;
+        }
+        
+        attempts += 1; // We now have an valid non duplicate guess so add 1
+        println!("\nYour guess is: {guess}");
+
+        // Push the guess to the vector and sort it in numeric order
         guessed_numbers.push(guess);
         guessed_numbers.sort();
 
         // compare the secret number to the guess and print result.
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Greater => println!("Too big!\n"),
             Ordering::Equal => {
-                println!("You won in {attempts} attempts!");
+                println!("You won, in {attempts} attempts!");
                 break;
             }
         };
 
-        println!("Your guesses are: {:?}", guessed_numbers);
+        println!("Your guesses are: {:?}\n", guessed_numbers);
     }
 }
